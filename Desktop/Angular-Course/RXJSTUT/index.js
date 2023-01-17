66fc06fe3b142a9d8619d33ee45278d863eddcce
Victor-negosek <victoript@gmail.com>
2023-01-17 21:25:06 +0100

    err and commit

--- Desktop/Angular-Course/RXJSTUT/index.js
+++ Desktop/Angular-Course/RXJSTUT/index.js
@@ -13,23 +13,38 @@ const users = {
   ],
 };
 
+const users2 = {
+  data: [
+    { status: "inactive", age: 18 },
+    { status: "inactive", age: 17 },
+    { status: "active", age: 32 },
+    { status: "active", age: 12 },
+    { status: "active", age: 25 },
+    { status: "inactive", age: 43 },
+    { status: "active", age: 20 },
+  ],
+};
+
 const observable = new Observable((subscriber) => {
-  subscriber.next(users);
+  subscriber.next(users2);
+  subscriber.complete();
+
+  subscriber.next(users2);
 }).pipe(
   map((value) => {
-    console.log("1) Got data from Observable", value);
+    // console.log("1) Got data from Observable", value);
     return value.data;
   }),
   map((value) => {
-    console.log("2) Got data from first operator", value);
+    // console.log("2) Got data from first operator", value);
     return value.filter((user) => user.status === "active");
   }),
   map((value) => {
-    console.log("3) Got data from seconde operator", value);
+    // console.log("3) Got data from seconde operator", value);
     return value.reduce((sum, user) => sum + user.age, 0) / value.length;
   }),
   map((value) => {
-    console.log("4) Got data from third operator", value);
+    // console.log("4) Got data from third operator", value);
     if (value < 18) throw new Error("Evarage age is too young");
     else return value;
   })
@@ -43,7 +58,7 @@ const observer = {
     console.log("Observer got an error of " + err);
   },
   complete: () => {
-    console.log("Observer goa a complete notification ");
+    console.log("Observer got a complete notification ");
   },
 };
 
