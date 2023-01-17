1d8747bdc2bfa7c9f6e06ff25a4ee7819003f398
Victor-negosek <victoript@gmail.com>
2023-01-17 21:18:07 +0100

    pipe implementation

--- Desktop/Angular-Course/RXJSTUT/index.js
+++ Desktop/Angular-Course/RXJSTUT/index.js
@@ -5,17 +5,35 @@ const users = {
   data: [
     { status: "inactive", age: 14 },
     { status: "inactive", age: 17 },
-    { status: "inactive", age: 32 },
+    { status: "active", age: 10 },
     { status: "active", age: 12 },
-    { status: "active", age: 25 },
+    { status: "active", age: 10 },
     { status: "inactive", age: 43 },
-    { status: "active", age: 25 },
+    { status: "active", age: 20 },
   ],
 };
 
 const observable = new Observable((subscriber) => {
   subscriber.next(users);
-}).pipe();
+}).pipe(
+  map((value) => {
+    console.log("1) Got data from Observable", value);
+    return value.data;
+  }),
+  map((value) => {
+    console.log("2) Got data from first operator", value);
+    return value.filter((user) => user.status === "active");
+  }),
+  map((value) => {
+    console.log("3) Got data from seconde operator", value);
+    return value.reduce((sum, user) => sum + user.age, 0) / value.length;
+  }),
+  map((value) => {
+    console.log("4) Got data from third operator", value);
+    if (value < 18) throw new Error("Evarage age is too young");
+    else return value;
+  })
+);
 
 const observer = {
   next: (value) => {
